fd596ff2bc02145acd5fcdc16f94656870869ac4
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-11-20 21:16:47 +0900

    mysql 내 아이디로 설정

--- src/main/java/com/nohhb/board/BoardApplication.java
+++ src/main/java/com/nohhb/board/BoardApplication.java
@@ -4,15 +4,18 @@ import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApplication implements CommandLineRunner {
-
     @Autowired
     EntityManagerFactory emf;
     public static void main(String[] args) {
-        SpringApplication.run(BoardApplication.class, args);
+        SpringApplication app = new SpringApplication(BoardApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+
     }
 
     @Override
