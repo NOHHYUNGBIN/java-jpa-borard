a20f9441a04d32cef2db1e84b549065149eabc06
NOHHYUNGBIN <shgudqlsz@naver.com>
2024-11-30 21:22:56 +0900

    pom.xml 수정

--- src/test/java/com/nohhb/board/BoardRepositoryTest4.java
+++ src/test/java/com/nohhb/board/BoardRepositoryTest4.java
@@ -1,5 +1,7 @@
 package com.nohhb.board;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
@@ -37,6 +39,47 @@ class BoardRepositoryTest4 {
             board.setUpDate(new Date());
             boardRepository.save(board);
         }
+    }
+    @Test
+    @DisplayName("querydsl로 쿼리 작성테스트3 - 동적 쿼리작성")
+    public void querydslTest3() {
+        String searchBy = "TC";
+        String keyword = "1";
+        keyword = "%" + keyword + "%";
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // 동적으로 조건을 다르게
+        if(searchBy.equalsIgnoreCase("T")) {
+            builder.and(board.title.like(keyword));
+        } else if(searchBy.equalsIgnoreCase("C")) {
+            builder.and(board.content.like(keyword));
+        } else if(searchBy.equalsIgnoreCase("TC")) {
+            builder.and(board.title.like(keyword)).or(board.content.like(keyword));
+        }
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+        JPAQuery<Board> query = qf.selectFrom(board)
+                .where(builder)
+                .orderBy(board.upDate.desc());
+        List<Board> list =  query.fetch();
+        list.forEach(System.out::println);
+    }
+    @Test
+    @DisplayName("querydsl로 쿼리 작성테스트2 - 복잡한 쿼리작성")
+    public void querydslTest2() {
+        JPAQueryFactory qf = new JPAQueryFactory(em);
+
+        JPAQuery<Tuple> query = qf.select(board.writer, board.viewCnt.sum()).from(board)
+                .where(board.title.notLike("title1%"))
+                .where(board.writer.eq("writer1"))
+                .where(board.content.contains("content"))
+                .where(board.content.isNotNull())
+                .groupBy(board.writer)
+                .having(board.viewCnt.sum().gt(100))
+                .orderBy(board.writer.asc())
+                .orderBy(board.viewCnt.sum().desc());
+
+        List<Tuple> list = query.fetch();
+        list.forEach(System.out::println);
     }
     @Test
     @DisplayName("querydsl로 쿼리 작성테스트1 - 간단한 쿼리작성")
